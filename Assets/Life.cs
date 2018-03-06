cf44aa2c644c1969c0fa1db16bbf9ce7d6c23d24
S0mbiy <alvaradoomar55@hotmail.com>
2018-03-05 19:32:32 -0600

    Vida y cronometro

---
+++ Assets/Life.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour {
+    public Image lifeIcon;
+    private int life;
+    // Use this for initialization
+    private void Awake()
+    {
+
+        PlayerPrefs.SetInt("Player Life", 3);
+    }
+    void Start () {
+        life = PlayerPrefs.GetInt("Player Life");
+        for(int i = 0; i < life; i++)
+        {
+            Image lifeI = Instantiate(lifeIcon,transform);
+            lifeI.transform.SetParent(gameObject.transform,false);
+            lifeI.rectTransform.position = new Vector3(20 + 55 * i, 348, 0);
+        }
+        AddLife();
+    }
+	void AddLife()
+    {
+        int life = PlayerPrefs.GetInt("Player Life");
+        if (life < 5)
+        {
+            PlayerPrefs.SetInt("Player Life", life+1);
+            Image lifeI = Instantiate(lifeIcon, transform);
+            lifeI.transform.SetParent(gameObject.transform, false);
+            lifeI.rectTransform.position = new Vector3(20 + 55 * life, 348, 0);
+        }
+    }
+    void RemoveLife()
+    {
+        int life = PlayerPrefs.GetInt("Player Life");
+        if (life > 0)
+        {
+            PlayerPrefs.SetInt("Player Life", --life);
+            Image[] vidas=gameObject.GetComponentsInChildren<Image>();
+            Destroy(vidas[vidas.Length - 1]);
+        }
+       
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text tiempo;
+    private int seconds,minutes;
+
+	// Use this for initialization
+	void Start () {
+        minutes = 0;
+        seconds = 0;
+        tiempo.text = "0:00";
+        StartCoroutine(Time());
+	}
+
+    IEnumerator Time()
+    {
+        while (true) {
+            seconds++;
+            seconds %= 60;
+            if (seconds == 0)
+            {
+                minutes++;
+            }
+            
+            tiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+    public void StopTime()
+    {
+        StopCoroutine(Time());
+    }
+}
