f3f85086864db656e664754b7ec8b7b8df06c4c9
Cesar Augusto <cesarau04@gmail.com>
2018-03-06 11:05:01 -0600

    Added courutine to wait for seconds instead of click

---
+++ Assets/EventListener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour {
+
+    private Renderer __render;
+	// Use this for initialization
+	void Start () {
+        __render = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onEnter()
+    {
+        __render.material.color = Color.red;
+    }
+
+    public void onExit()
+    {
+        __render.material.color = Color.white;
+    }
+
+    public void onGrab()
+    {
+        Transform trans = GvrPointerInputModule.Pointer.PointerTransform;
+        trans.SetParent(trans, false);
+        trans.localPosition = Vector3.zero;
+    }
+
+    public void onRelease()
+    {
+        transform.SetParent(null, true);
+    }
+}
--- Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
+++ Assets/GoogleVR/Demos/Scripts/HelloVR/ObjectController.cs
@@ -13,10 +13,11 @@
 // limitations under the License.
 
 namespace GoogleVR.HelloVR {
-  using UnityEngine;
+using UnityEngine;
+using System.Collections;
 
-  [RequireComponent(typeof(Collider))]
-  public class ObjectController : MonoBehaviour {
+[RequireComponent(typeof(Collider))]
+public class ObjectController : MonoBehaviour {
     private Vector3 startingPosition;
     private Renderer renderer;
 
@@ -24,61 +25,73 @@ namespace GoogleVR.HelloVR {
     public Material gazedAtMaterial;
 
     void Start() {
-      startingPosition = transform.localPosition;
-      renderer = GetComponent<Renderer>();
-      SetGazedAt(false);
+        startingPosition = transform.localPosition;
+        renderer = GetComponent<Renderer>();
+        SetGazedAt(false);
+}
+
+    IEnumerator click()
+    {
+        yield return new WaitForSeconds(3);
+        TeleportRandomly();
     }
+    
 
     public void SetGazedAt(bool gazedAt) {
-      if (inactiveMaterial != null && gazedAtMaterial != null) {
-        renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
-        return;
-      }
+        if (inactiveMaterial != null && gazedAtMaterial != null) {
+            renderer.material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            StartCoroutine(click());
+            return;
+        }
+        else
+        {
+            StopCoroutine(click());
+        }
     }
 
     public void Reset() {
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      for (int i=0; i<numSibs; i++) {
+        int sibIdx = transform.GetSiblingIndex();
+        int numSibs = transform.parent.childCount;
+        for (int i=0; i<numSibs; i++) {
         GameObject sib = transform.parent.GetChild(i).gameObject;
         sib.transform.localPosition = startingPosition;
         sib.SetActive(i == sibIdx);
-      }
     }
+}
 
     public void Recenter() {
-#if !UNITY_EDITOR
-      GvrCardboardHelpers.Recenter();
-#else
-      if (GvrEditorEmulator.Instance != null) {
-        GvrEditorEmulator.Instance.Recenter();
-      }
-#endif  // !UNITY_EDITOR
+        #if !UNITY_EDITOR
+        GvrCardboardHelpers.Recenter();
+        #else
+        if (GvrEditorEmulator.Instance != null) {
+            GvrEditorEmulator.Instance.Recenter();
+        }
+        #endif  // !UNITY_EDITOR
     }
 
     public void TeleportRandomly() {
-      // Pick a random sibling, move them somewhere random, activate them,
-      // deactivate ourself.
-      int sibIdx = transform.GetSiblingIndex();
-      int numSibs = transform.parent.childCount;
-      sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-      GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+        // Pick a random sibling, move them somewhere random, activate them,
+        // deactivate ourself.
+        int sibIdx = transform.GetSiblingIndex();
+        int numSibs = transform.parent.childCount;
+        sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
+        GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
 
-      // Move to random new location ±100º horzontal.
-      Vector3 direction = Quaternion.Euler(
-          0,
-          Random.Range(-90, 90),
-          0) * Vector3.forward;
-      // New location between 1.5m and 3.5m.
-      float distance = 2 * Random.value + 1.5f;
-      Vector3 newPos = direction * distance;
-      // Limit vertical position to be fully in the room.
-      newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
-      randomSib.transform.localPosition = newPos;
+        // Move to random new location ±100º horzontal.
+        Vector3 direction = Quaternion.Euler(
+            0,
+            Random.Range(-90, 90),
+            0) * Vector3.forward;
+        // New location between 1.5m and 3.5m.
+        float distance = 2 * Random.value + 1.5f;
+        Vector3 newPos = direction * distance;
+        // Limit vertical position to be fully in the room.
+        newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
+        randomSib.transform.localPosition = newPos;
 
-      randomSib.SetActive(true);
-      gameObject.SetActive(false);
-      SetGazedAt(false);
+        randomSib.SetActive(true);
+        gameObject.SetActive(false);
+        SetGazedAt(false);
+        }
     }
-  }
 }
